caa8774c16ab51da0b78b220262d1293964397ec
emrosesajib <emrose8981@gmail.com>
2026-02-11 21:56:17 +0600

    brushup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,7 +13,6 @@ export function createApp() {
   const allowedOrigins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://dekleineman-indianrestaurant.nl/",
     "https://restaurent-mern.vercel.app",
     // add your deployed frontend domain too later
   ];
